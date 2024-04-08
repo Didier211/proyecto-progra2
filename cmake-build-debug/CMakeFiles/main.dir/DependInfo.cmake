1be7a0b462d47151966f5904fb26c1e9e0c32cf5
Didier211 <didier.miranda.delgado@est.una.ac.cr>
2024-04-08 10:44:10 -0600

    fix

---
+++ cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/Abarrote.cpp" "CMakeFiles/main.dir/src/Abarrote.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Abarrote.cpp.obj.d"
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/Carne.cpp" "CMakeFiles/main.dir/src/Carne.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Carne.cpp.obj.d"
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/Categoria.cpp" "CMakeFiles/main.dir/src/Categoria.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Categoria.cpp.obj.d"
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/Conserva.cpp" "CMakeFiles/main.dir/src/Conserva.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Conserva.cpp.obj.d"
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/Embutido.cpp" "CMakeFiles/main.dir/src/Embutido.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Embutido.cpp.obj.d"
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/Empaque.cpp" "CMakeFiles/main.dir/src/Empaque.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Empaque.cpp.obj.d"
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/Fecha.cpp" "CMakeFiles/main.dir/src/Fecha.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Fecha.cpp.obj.d"
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/Perecedero.cpp" "CMakeFiles/main.dir/src/Perecedero.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Perecedero.cpp.obj.d"
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/Producto.cpp" "CMakeFiles/main.dir/src/Producto.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Producto.cpp.obj.d"
+  "C:/Users/Didier/OneDrive/Escritorio/proyecto-progra2/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.obj" "gcc" "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/Abarrote.cpp.obj"
+  "CMakeFiles/main.dir/src/Abarrote.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Carne.cpp.obj"
+  "CMakeFiles/main.dir/src/Carne.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Categoria.cpp.obj"
+  "CMakeFiles/main.dir/src/Categoria.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Conserva.cpp.obj"
+  "CMakeFiles/main.dir/src/Conserva.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Embutido.cpp.obj"
+  "CMakeFiles/main.dir/src/Embutido.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Empaque.cpp.obj"
+  "CMakeFiles/main.dir/src/Empaque.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Fecha.cpp.obj"
+  "CMakeFiles/main.dir/src/Fecha.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Perecedero.cpp.obj"
+  "CMakeFiles/main.dir/src/Perecedero.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Producto.cpp.obj"
+  "CMakeFiles/main.dir/src/Producto.cpp.obj.d"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "libmain.dll.a"
+  "main.exe"
+  "main.exe.manifest"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
